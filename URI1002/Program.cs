a2784766756bda4c3e3c73b92ff168c42f3ed90e
Evandro2270 <evandro535@gmail.com>
2024-07-12 15:01:38 -0300

    Adicionar arquivos de projeto.

---
+++ URI1002/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+namespace Programa
+{
+    using System;
+
+    namespace URI
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                double R, A, pi = 3.14159;
+
+                R = double.Parse(Console.ReadLine(), CI);
+
+                A = pi * R * R;
+
+                Console.WriteLine("A=" + A.ToString("F4", CI));
+            }
+        }
+    }
+}
